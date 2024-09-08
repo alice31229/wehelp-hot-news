51a50b1e3904fc19fd23db2ecb9fbcbd9337c054
Alice Chen <alice31229@gmail.com>
2024-09-08 19:37:36 +0800

    comments optimize

--- webScrape/cronjob.py
+++ webScrape/cronjob.py
@@ -11,25 +11,25 @@ from webScrape.businesstoday import get_businesstoday
 def main():
     scheduler = sched.scheduler(time.time, time.sleep)
 
-    # 每晚12點執行爬虫1和爬虫2
+    # 每晚12點執行四支爬蟲程式
     scheduler.enter(0, 1, get_pttbrain, ())
     scheduler.enter(0, 1, get_udn, ())
     scheduler.enter(0, 1, get_storm, ())
     scheduler.enter(0, 1, get_businesstoday, ())
 
-    # 创建两个线程
+    # 四個流程
     t1 = threading.Thread(target=scheduler.run)
     t2 = threading.Thread(target=scheduler.run)
     t3 = threading.Thread(target=scheduler.run)
     t4 = threading.Thread(target=scheduler.run)
 
-    # 启动线程
+    # 啟動流程
     t1.start()
     t2.start()
     t3.start()
     t4.start()
 
-    # 等待线程结束
+    # 等待流程結束
     t1.join()
     t2.join()
     t3.join()
